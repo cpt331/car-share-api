2b0e4cf57514a93a824a61ec10e85a7a8e5f1a33
Steven Innes <s3355537@student.rmit.edu.au>
2017-09-15 16:41:45 +1000

    started added register front end demo page

---
+++ CarShareApi/Scripts/Controllers/RegisterController.js
@@ -0,0 +1,7 @@
+﻿
+angular.module('CarShareApp').controller('RegisterController', function ($scope, $rootScope ,$http) {
+    // create a message to display in our view
+    $scope.Loading = false;
+    $scope.RegisterRequest = { Email: '', Password: '', ConfirmPassword: '' };
+    
+});
